59ec3b6170f0805411b659e5c46f5e42b3545b13
Ben Tanen <ben.tanen@gmail.com>
2016-05-02 12:52:42 -0400

    cleaned up svm

--- svm-poems/readme.md
+++ svm-poems/readme.md
@@ -1,9 +1,11 @@
 ### SVM Poem Categorization
 
-Small assignment to test uses of support vector machines in caterogizing exercpts of poems in different languages. The ten poems used here are in English and German. The exercpts used were all less than or equal to 50 characters (to include full words only).
+Small assignment to test uses of support vector machines in categorizing excerpts of poems in different languages. The ten poems used here are in English and German. The excerpts used were all less than or equal to 50 characters (to include full words only).
 
-The catergoization of the poems is based on the number of words in each exercpt and the number of times the letter 'i' occurs.
+The categorization of the poems is based on the number of words in each excerpt and the number of times the letter 'i' occurs.
 
 ![Example Output](output.png)
+
+The original ten poems can be seen plotted in red and blue (for English and German respectively). A new test poem is then plotted in magenta. This poem is obviously German so it appears the SVM works correctly.
 
 This was work from Tufts' MATH-150 course (Spring 2016).
--- svm-poems/svm_poems.m
+++ svm-poems/svm_poems.m
@@ -16,10 +16,10 @@ poems  = {'I cannot live with you, It would be life, And life', ...
           'An einem Sommermorgen da nimm den Wanderstab, es',   ...
           'Aktenstose nachts verschlingen, Schwatzen nach der Welt'};
 
+% calculate features of poems
 poem_sizes = zeros(n,1);
 i_count    = zeros(n,1);
 for i = 1:n
-    i
     poem_sizes(i) = size(cell2mat(strfind(poems(i),' ')),2) + 1;
     i_count(i)    = size(cell2mat(strfind(poems(i),'i')),2) + ...
                     size(cell2mat(strfind(poems(i),'I')),2);
@@ -29,17 +29,20 @@ x = poem_sizes;
 y = i_count;
 c = [-1, -1, -1, -1, -1,  1,  1, 1, 1, 1];
 
+% plot data
 hold on
 plot(x(1:5), y(1:5),  '.r', 'Markersize', 25)
 plot(x(6:10),y(6:10), '.b', 'Markersize', 25)
-xlim([5, 15]);
-ylim([0, 7]);
+title('Categorizing German vs. English Poems');
 ylabel('number of "i"');
 xlabel('number of complete words');
+xlim([5, 15]);
+ylim([0, 7]);
 
 z_0= [1; 0; 0];
 C  = 1;
 
+% perform operation of SVM
 z=fminunc(@F,z_0);
 w=[z(1); z(2)];
 b=z(3);
@@ -48,7 +51,15 @@ yy(1)=(-b-xx(1)*w(1))/w(2);
 xx(2)=0;
 yy(2)=(-b-xx(2)*w(1))/w(2);
 
+% plot line on chart
 plot(xx,yy,'-g','Linewidth',1);
-plot(7,3,'.m', 'Markersize', 25);
-title('Categorizing German vs. English Poems')
+
+% testing SVM by plotting new poem in magenta
+% poem is obviously in German - should be on blue side of partition
+new_poem = 'Hat der alte Hexenmeister sich doch einmal wegbege';
+new_poem_x = size(cell2mat(strfind(poems(i),' ')),2) + 1;
+new_poem_y = size(cell2mat(strfind(poems(i),'i')),2) + ...
+             size(cell2mat(strfind(poems(i),'I')),2);
+plot(new_poem_x,new_poem_y,'.m', 'Markersize', 25);
+
 hold off;
